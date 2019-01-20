fc9a873445a9a4711f92d28e397396168c41ffa8
pritishvaidya <pritishvaidya94@gmail.com>
2019-01-20 22:11:48 +0530

    Adding the tests :diamonds:

---
+++ test.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+import voidReactNativeTags from './void';
+import reactNativeTags from '.';
+
+test('`react-native-tags` is an array of React Native string tags', t => {
+	t.true(Array.isArray(reactNativeTags));
+	t.true(reactNativeTags.length > 10 && reactNativeTags.length < 1000);
+
+	reactNativeTags.forEach(tagName => {
+		t.true(typeof tagName === 'string');
+	});
+});
+
+test('`react-native-tags/void` is an array of void React Native Void string tags', t => {
+	t.true(Array.isArray(voidReactNativeTags));
+	t.true(voidReactNativeTags.length > 10 && voidReactNativeTags.length < 1000);
+
+	reactNativeTags.forEach(tagName => {
+		t.true(typeof tagName === 'string');
+	});
+});
